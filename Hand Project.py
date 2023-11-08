13281b7c7ebbd736336857ff5078dab769db0de7
Chehoslovakiya <sunrise60022006@gmail.com>
2023-11-08 18:49:53 +0200

    Core Hand Tracking files

---
+++ Hand Project.py
@@ -0,0 +1,149 @@
+import cv2    
+import time
+import numpy as np
+import HandTrackingModule as htm
+import ctypes
+import math
+import threading
+from comtypes import CLSCTX_ALL
+from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
+
+wCam, hCam = 640, 480
+
+cap = cv2.VideoCapture(0)
+cap.set(3, wCam)
+cap.set(4, hCam)
+detector = htm.handDetector(detectionCon=0.8)
+
+devices = AudioUtilities.GetSpeakers()
+interface = devices.Activate(
+    IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+volume = interface.QueryInterface(IAudioEndpointVolume)
+vol_range=volume.GetVolumeRange()
+volume.SetMasterVolumeLevel(0,None)
+min_vol = -65
+max_vol = 0
+vol_bar= 0
+first_iteration = True
+vol = 0
+vol_p = 0
+vol_lock = threading.Lock()
+time_condition_met = None
+
+def volume_save():
+    global vol
+    global vol_p
+    while True:
+        time.sleep(2)
+        with vol_lock:
+            vol_p = vol
+              
+    
+def volume_changing(img, lmList):
+    
+    global first_iteration
+    global vol
+    global vol_p
+    global time_condition_met
+    if len(lmList) != 0  :
+        
+        #print(lmList[4],lmList[8])
+        x1, y1 = lmList[4][1] , lmList[4][2]
+        x2, y2 = lmList[8][1] , lmList[8][2]
+        cx,cy = (x1+x2)//2, (y1+y2)//2
+        length =math.hypot(x2-x1,y2-y1)
+        if length < 50 and first_iteration:
+            cv2.circle(img, (x1,y1), 10, (255,0,255), cv2.FILLED )
+            cv2.circle(img, (x2,y2), 10, (255,0,255), cv2.FILLED )
+            cv2.circle(img, (cx,cy), 5, (255,0,255), cv2.FILLED )
+            cv2.line(img, (x1,y1),(x2,y2),(0,165,255),3)
+            #print(length)
+
+            # Hand Range 50 - 300
+            # volume range -65 - 0
+
+            vol = np.interp(length,[50,300],[min_vol, max_vol])
+            vol_bar = np.interp(length,[50,300],[475,155])
+            #print(int(length),vol)
+            volume.SetMasterVolumeLevel(vol, None)
+            cv2.rectangle(img, (5,int(vol_bar)), (35,475), (250, 0, 250), cv2.FILLED )
+            if vol==-65:
+                cv2.circle(img, (cx,cy), 5, (0,255,0), cv2.FILLED )
+            if vol==0:
+                cv2.circle(img, (x1,y1), 10, (0,0,255), cv2.FILLED )
+                cv2.circle(img, (x2,y2), 10, (0,0,255), cv2.FILLED )
+            first_iteration = False    
+            
+            return vol , first_iteration 
+        
+        if  first_iteration != True :
+            x1, y1 = lmList[4][1] , lmList[4][2]
+            x2, y2 = lmList[8][1] , lmList[8][2]
+            cx,cy = (x1+x2)//2, (y1+y2)//2
+            length =math.hypot(x2-x1,y2-y1)
+            #print(length)
+            cv2.circle(img, (x1,y1), 10, (255,0,255), cv2.FILLED )
+            cv2.circle(img, (x2,y2), 10, (255,0,255), cv2.FILLED )
+            cv2.circle(img, (cx,cy), 5, (255,0,255), cv2.FILLED )
+            cv2.line(img, (x1,y1),(x2,y2),(0,165,255),3)
+            #print(length)
+
+            # Hand Range 50 - 300
+            # volume range -65 - 0
+
+            vol = np.interp(length,[50,300],[min_vol, max_vol])
+            vol_bar = np.interp(length,[50,300],[475,155])
+            #print(int(length),vol)
+            volume.SetMasterVolumeLevel(vol, None)
+            cv2.rectangle(img, (5,int(vol_bar)), (35,475), (250, 0, 250), cv2.FILLED )
+            if vol==-65:
+                cv2.circle(img, (cx,cy), 5, (0,255,0), cv2.FILLED )
+            if vol==0:
+                cv2.circle(img, (x1,y1), 10, (0,0,255), cv2.FILLED )
+                cv2.circle(img, (x2,y2), 10, (0,0,255), cv2.FILLED )
+            if  abs(vol - vol_p) < 10 :
+                if time_condition_met is None:
+                    time_condition_met = time.time()
+                # перевіряє чи виконується умова 2 сек
+                elif time.time() - time_condition_met >= 2:
+                    first_iteration = True
+                    time_condition_met = None  # збиває таймер
+            else:
+                # якщо умова не виконана збиває таймер
+                time_condition_met = None 
+            return vol
+        
+           
+            
+
+
+
+     
+volume_save_thread = threading.Thread(target=volume_save, daemon=True)
+volume_save_thread.start()      
+    
+while True:
+    success, img = cap.read()
+    img = detector.findHands(img)
+    #fps_img = detector.Fps_m(img)
+    lmList = detector.findPosition(img)
+    
+    volume_c=volume_changing(img, lmList)
+    #print(vol_p)
+  
+    
+    cv2.rectangle(img, (0,150), (40, 480), (250, 250, 250), 5 )
+
+
+    cv2.imshow("img", img)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+    if cv2.getWindowProperty("img", cv2.WND_PROP_VISIBLE)<1:
+        break
+    cv2.waitKey(1) 
+
+
+
+    
+
+
---
+++ HandTrackingModule.py
@@ -0,0 +1,93 @@
+import cv2 
+import  mediapipe as mp     
+import time
+
+
+
+class handDetector():
+    def __init__(self,mode=False,maxHands=2,modelC=1,detectionCon=0.5,trackCon=0.5):
+            self.cTime = 0  # Initialize cTime
+            self.pTime = 0
+            self.mode = mode 
+            self.maxHands = maxHands
+            self.modelC=modelC
+            self.detectionCon = detectionCon
+            self.trackCon = trackCon
+            
+            self.mpHands = mp.solutions.hands
+            self.hands = self.mpHands.Hands(self.mode, self.maxHands,self.modelC, self.detectionCon, self.trackCon)
+            self.mpDraw = mp.solutions.drawing_utils #математичні обчислення щоб нанести на руку точки
+
+    def findHands(self, img , draw = True):
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        self.results = self.hands.process(imgRGB)
+        #print(results.multi_hand_landmarks)
+     
+        if self.results.multi_hand_landmarks:
+            for hand_Landmarks in self.results.multi_hand_landmarks:
+                if draw:
+                    self.mpDraw.draw_landmarks(img, hand_Landmarks, self.mpHands.HAND_CONNECTIONS) #малює точки
+        return img
+    
+
+    def findPosition(self, img, handN=0, draw=True):
+        lmList = []
+
+        if self.results.multi_hand_landmarks:
+            my_Hand = self.results.multi_hand_landmarks[handN]
+         
+            for id, lm in enumerate(my_Hand.landmark):
+                #print(id,lm)   
+                h, w, c = img.shape
+                cx = int(lm.x*w)
+                cy = int(lm.y*h)
+                lmList.append([id, cx, cy])
+                
+
+        return lmList
+                     
+                     #print(0,".", cx, cy , cz) 
+                     #if id == 0:
+                         #cv2.circle(img, (cx, cy), 15, (255,0,255), cv2.FILLED )
+    def Fps_m(self, img, cTime=0, pTime=0):
+
+        self.cTime = time.time() #loop для фпс
+        fps = 1/(self.cTime-self.pTime)
+        self.pTime = self.cTime
+
+        img_fps=cv2.putText(img, "Fps: " + str(int(fps)),(10,40) ,cv2.FONT_HERSHEY_PLAIN,2,(255,0,255),3)
+
+        return img_fps
+        
+ 
+
+
+def main():
+   
+ 
+   cap =cv2.VideoCapture(0)
+     
+   detector = handDetector()
+
+   while True:
+    success, img = cap.read()
+    
+    img = detector.findHands(img) #надсилаю імг в метод класу детектор руки на обробку і повертаю.
+    lmList = detector.findPosition(img)
+    
+    if len(lmList) !=0:
+      print(lmList[4])
+
+    Fps = detector.Fps_m(img)
+    
+    cv2.imshow("image", img)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+     break
+    if cv2.getWindowProperty("image", cv2.WND_PROP_VISIBLE)<1:
+        break
+    cv2.waitKey(1)
+
+
+
+if __name__ == "__main__":
+    main()
